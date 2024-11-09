f784250b7c04825536ffdab8cdaec766a3cfa576
cshinagawa <c.shinagawa@wiz-net.jp>
2024-11-10 00:11:23 +0900

    ユーザー認証機能を追加し、Todoアイテムの削除機能を実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// AWS Cognitoと連携してユーザー認証機能を実装するためのコンポーネント
+import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator>{/* // ラップすることでユーザー認証機能を有効化 */}
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
